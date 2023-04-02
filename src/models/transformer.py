f7236cda2968d84071ea62c6158fc52eefcd22af
Simon Ekman <simonevh@kth.se>
2023-04-02 10:34:59 +0200

    feat(transformer): prepend class token

--- src/models/transformer.py
+++ src/models/transformer.py
@@ -2,7 +2,7 @@ import math
 
 import torch
 from pytorch_lightning import LightningModule
-from torch import Tensor, zeros
+from torch import Tensor, zeros, sigmoid, nn
 from torch.nn import Dropout, Linear, TransformerEncoderLayer, TransformerEncoder, BCEWithLogitsLoss, Module
 from torch.optim import AdamW
 from torch.optim.lr_scheduler import CosineAnnealingLR
@@ -35,6 +35,27 @@ class PositionalEncoding(Module):
         return self.dropout(x)
 
 
+class PrependClassToken(nn.Module):
+    """
+    PrependClassToken Class:
+    Module to prepend a learnable class token to every sequence in batch.
+    """
+
+    def __init__(self, d_model: int):
+        super().__init__()
+        # The class token does not carry any information in itself. The hidden state corresponding to this token at the
+        # end of the transformer will be inferred by all other tokens in the sequence.
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, d_model), requires_grad=True)
+        nn.init.normal_(self.cls_token, std=1e-6)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        :param x: dimensions [batch_size, seq_len, embedding_dim]
+        :return: x prepended with class token [batch_size, seq_len+1, embedding_dim]
+        """
+        return torch.cat((self.cls_token.expand(x.shape[0], -1, -1), x), dim=1)
+
+
 class TransformerClassifier(Module):
 
     def __init__(self, *, in_features: int = 4, num_layers: int = 4, n_classes: int = 2, **tf_kwargs):
@@ -50,15 +71,18 @@ class TransformerClassifier(Module):
         )
         self.classifier = Linear(tf_kwargs['d_model'], n_classes)
         self.d_model = tf_kwargs['d_model']
+        self.prepend_cls_token = PrependClassToken(self.d_model)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # Project every item in the sequence to the dimensionality of the transformer encoder
         x = self.projector(x)
+        # Prepend a class token to the sequence
+        x = self.prepend_cls_token(x)
         # Apply positional encoding
         x = self.pos_encoder(x)
         x = self.transformer_encoder(x)
         # return self.decoder(x[:, 0])
-        return self.classifier(x.mean(dim=1)).squeeze()
+        return self.classifier(x[:, 0]).squeeze()
 
 
 class LitTimeSeriesClassifier(LightningModule):
@@ -69,6 +93,7 @@ class LitTimeSeriesClassifier(LightningModule):
     def __init__(self, encoder, decoder, feature_dim, lr, wd):
         super(LitTimeSeriesClassifier, self).__init__()
         self.encoder = encoder
+        self.encoder.freeze()
         self.decoder = decoder
         self.feature_dim = feature_dim
         self.lr = lr
@@ -81,6 +106,7 @@ class LitTimeSeriesClassifier(LightningModule):
         :param x: A tensor of shape (N, L, M, T), where N is the batch size and L is the number of segments in the trial
         :return: A tensor of shape (N) with logits
         """
+
         n, l, m, t = x.shape
         tokens = zeros(n, l, self.feature_dim).to(self.device)
         for i in range(n):
@@ -89,29 +115,40 @@ class LitTimeSeriesClassifier(LightningModule):
 
         return self.decoder(tokens)
 
-    def training_step(self, batch, batch_idx):
+    def shared_step(self, batch, batch_idx, step):
         logits = self(batch.x)
         loss = self.loss_fn(logits, batch.y)
-        self.log_dict({'train_loss': loss,
-                       'train_accuracy': binary_accuracy(logits, batch.y.long()),
-                       'train_f1': f1_score(logits, batch.y.long(), task='binary', average='macro')}, on_epoch=True)
+        probs = sigmoid(logits)
+
+        self.log_dict({
+            f'{step}_loss': loss,
+            # The accuracy for the predictions
+            f'{step}_accuracy': binary_accuracy(probs, batch.y),
+            # The F1 score for the predictions
+            f'{step}_f1': f1_score(probs, batch.y, task='binary', average='macro')},
+            on_epoch=True)
+
         return loss
 
-    def validation_step(self, batch, batch_idx):
-        logits = self(batch.x)
-        loss = self.loss_fn(logits, batch.y)
-        self.log_dict({'val_loss': loss,
-                       'val_accuracy': binary_accuracy(logits, batch.y.long()),
-                       'val_f1': f1_score(logits, batch.y.long(), task='binary', average='macro')})
-        return loss
+    def training_step(self, *args):
+        return self.shared_step(*args, step='train')
+
+    def validation_step(self, *args):
+        return self.shared_step(*args, step='val')
 
     def test_step(self, batch, batch_idx):
-        logits = self(batch.x)
-        loss = self.loss_fn(logits, batch.y)
-        self.log_dict({'test_loss': loss,
-                       'test_accuracy': binary_accuracy(logits, batch.y.long()),
-                       'test_f1': f1_score(logits, batch.y.long(), task='binary', average='macro')})
-        return loss
+        _, logits = self(batch)
+        probs = nn.functional.sigmoid(logits)
+
+        self.log_dict({
+            # The accuracy for the anchor predictions
+            f'test_accuracy': binary_accuracy(probs, batch.y),
+            # The F1 score for the anchor predictions
+            f'test_f1': f1_score(probs, batch.y, task='binary', average='macro')},
+        )
+
+        # Return the predictions for plotting confusion matrix
+        return probs.round()
 
     def configure_optimizers(self):
         optimizer = AdamW(self.parameters(), lr=self.lr, weight_decay=self.wd)
