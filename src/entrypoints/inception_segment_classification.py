ab7e968b47033d5b428014b1a7ab959a3f19bf58
Simon Ekman <simonevh@kth.se>
2023-04-03 20:59:07 +0200

    feat(inceptiontime): plot confusion matrix for segment classification

--- src/entrypoints/inception_segment_classification.py
+++ src/entrypoints/inception_segment_classification.py
@@ -1,8 +1,10 @@
+import matplotlib.pyplot as plt
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import NeptuneLogger
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score, accuracy_score
+from sklearn.manifold import TSNE
+from sklearn.metrics import f1_score, accuracy_score, ConfusionMatrixDisplay
 from yaml import load as load_yaml, FullLoader
 
 from datamodule import KIDataModule
@@ -10,6 +12,7 @@ from models.inceptiontime import LitInceptionTime
 from utils.const import SEED
 from utils.misc import set_random_state
 from utils.path import config_path, log_path, checkpoint_path
+from utils.visualize import visualize_latent_space
 
 
 def main():
@@ -41,7 +44,7 @@ def main():
 
     trainer.fit(model, datamodule=dm)
 
-    # model = LitInceptionTime.load_from_checkpoint(checkpoint_path.joinpath('epoch=49-step=200-v1.ckpt'))
+    # model = LitInceptionTime.load_from_checkpoint(checkpoint_path.joinpath('use_this.ckpt'))
     model = LitInceptionTime.load_from_checkpoint(checkpoint_callback.best_model_path)
 
     dm.set_use_triplets(False)
@@ -71,20 +74,27 @@ def main():
     train_embeddings = model(train_batch.x)
     forest_clf.fit(train_embeddings.detach(), train_batch.y)
 
-    # Visualize how many principal components are required to represent data
-    # plot_top_eigenvalues(val_features)
-
-    # Visualize the latent neighborhoods with TSNE
-    # tsne = TSNE(n_components=2, perplexity=50)
-    # visualize_latent_space(tsne, val_features, val_batch, labels)
-
     # Evaluate
     val_embeddings = model(val_batch.x)
     pred = forest_clf.predict(val_embeddings.detach())
 
+    # Visualize how many principal components are required to represent data
+    # plot_top_eigenvalues(val_features)
+
+    # Visualize the latent neighborhoods with TSNE
+    tsne = TSNE(n_components=2, perplexity=50)
+    manifold = tsne.fit_transform(val_embeddings.detach())
+    visualize_latent_space(manifold, val_batch.y, dm.class_names(), show=True)
+
     # Compute and return metric
     print(f"f1: {f1_score(val_batch.y, pred, average='macro')}")
     print(f"accuracy: {accuracy_score(val_batch.y, pred)}")
+
+    # in order to handle raise ValueError("At least one label specified must be in y_true")
+    figure = ConfusionMatrixDisplay.from_predictions(val_batch.y, pred, display_labels=dm.class_names(),
+                                                     cmap='Blues')
+    figure.ax_.set_title('Segment Classification for AntiSaccades')
+    plt.show()
 
     # Compute attribute importance
     attribute_importance = {
